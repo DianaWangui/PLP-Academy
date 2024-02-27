095d40d4a8d82bd8d6689ffaa642efc8fa8da843
DianaWangui <dianawanguikt048@gmail.com>
2024-02-27 13:12:11 +0300

    create a user input

---
+++ Python-projects/user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter yoyr name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your location: ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
